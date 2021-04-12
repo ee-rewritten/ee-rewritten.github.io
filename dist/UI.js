d818244c9fc94a43d4f7b17ed53507602e354a58
seb-135 <potoniecsebastian@gmail.com>
2021-04-12 01:27:12 +0100

    Improved FPS counter

--- dist/UI.js
+++ dist/UI.js
@@ -1,21 +1,33 @@
-class UI {
-  overlayContainer;
+class UI extends PIXI.Container {
   debugText;
+  fps = new Array(10);
 
   constructor() {
-    this.overlayContainer = new Container();
-
+    super();
     this.drawUIRect(1, -1, Config.fullWidth-Config.gameWidth, Config.fullHeight+2, Config.gameWidth, 0, 0x000000);
     this.drawUIRect(1, 1, Config.gameWidth, Config.fullHeight-Config.gameHeight-2, 0, Config.gameHeight, 0x323231);
 
     this.debugText = new PIXI.Text('FPS: xx',{fill: 'white', fontSize: 12});
-    this.overlayContainer.addChild(this.debugText);
-
-    Global.stage.addChild(this.overlayContainer);
+    this.addChild(this.debugText);
   }
 
   enterFrame() {
-    this.debugText.text = `FPS: ${Math.round(ticker.FPS)}`;
+    //overengineered fps counter let's go
+    let fps;
+    if(fps < 1) fps = ticker.FPS;
+    else {
+      this.fps.shift();
+      this.fps.push(ticker.FPS);
+      fps = this.fps.reduce((a,b) => a+b)/this.fps.length;
+      let diff = Math.abs(ticker.FPS-fps);
+
+      if(diff > 5)
+        for(let i; i < this.fps.length/2; i++) {
+          this.fps.shift();
+          this.fps.push(ticker.FPS);
+        }
+    }
+    this.debugText.text = fps < 1 ? `SPF: ${Math.gaussRound(1/fps,1)}` : `FPS: ${Math.gaussRound(fps,1)}`;
   }
 
   drawUIRect(localx, localy, width, height, x, y, colour) {
@@ -27,6 +39,6 @@ class UI {
     rect.y = y;
     rect.endFill();
     // rect.alpha = 0.5;
-    this.overlayContainer.addChild(rect);
+    this.addChild(rect);
   }
 }
